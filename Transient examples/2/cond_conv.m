1d4f95e1326a405cf84c00a50bfaa6bebb1a4c0f
Murtaza Vohra <41868594+murtazav@users.noreply.github.com>
2019-08-03 16:29:35 +0530

    Create cond_conv.m

---
+++ Transient examples/2/cond_conv.m
@@ -0,0 +1,33 @@
+dt = 1;
+dx = 0.01;
+tf=10000;
+t = 0:dt:tf;
+x = 0:dx:1;
+%copper
+g_dot=0.5;
+alpha = 1.11*(10^-4);
+T2 = 293;
+T1 = 400;
+T_inf = 320;
+K=401;
+h=63;
+rho=8960;
+Cp=0.385;
+for i=1:tf/dt
+    for j=1:100
+        if j ==1
+            T(i,j)=T1;
+        else
+            T(i,j)=T2;
+        end
+    end
+end
+for i=2:tf/dt
+    T(i,100) = T(i-1,100)+dt/dx*(alpha*(T(i-1,99)-T(i-1,100))+(h*(T_inf-T(i-1,100))+g_dot)/(rho*Cp));
+    for j=2:99
+        T(i,j)= T(i-1,j)+dt/dx*(alpha*(T(i-1,j-1)-2*T(i-1,j)+T(i-1,j+1))+g_dot/rho/Cp);
+    end
+end
+        
+
+
