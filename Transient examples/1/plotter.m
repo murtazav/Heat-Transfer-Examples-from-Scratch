a5c9f885f8213d81913283603d85356f1d7d5005
Murtaza Vohra <41868594+murtazav@users.noreply.github.com>
2019-08-03 16:14:41 +0530

    Add files via upload

---
+++ Transient examples/1/plotter.m
@@ -0,0 +1,13 @@
+% 
+% for i=1:1000:10000
+%     plot(x(1:100),T(i,:));
+%     hold on;
+% end;
+% xlabel('Length (in m)');
+% ylabel('Temperature in K');
+[xx,yy]=meshgrid(x(1:100),t(1:10000));
+surf(xx,yy,T)
+colormap('jet')
+shading interp
+xlabel('X (in m)')
+ylabel('Time (in s)')
